14ede96020811395eb7ac2b93e865810f7cfdfb4
MetaeaseLib <143768333+MetaeaseLib@users.noreply.github.com>
2023-12-05 08:18:53 +0000

    202312051718_認証実装

--- src/main/java/jp/co/metaease/web/ctrl/WebApi920101Controller.java
+++ src/main/java/jp/co/metaease/web/ctrl/WebApi920101Controller.java
@@ -16,12 +16,28 @@
 
 package jp.co.metaease.web.ctrl;
 
+import java.util.Map;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /** Example REST controller to demonstrate structured logging. */
 @RestController
@@ -35,11 +51,11 @@ public class WebApi920101Controller {
   @GetMapping("/")
   public @ResponseBody String index() {
     // Example of structured logging - add custom fields
-    MDC.put("logField", "custom-entry");
-    MDC.put("arbitraryField", "custom-entry");
+    MDC.put("logField", "index");
+    MDC.put("arbitraryField", "任意の領域");
     // Use logger with log correlation
     // https://cloud.google.com/run/docs/logging#correlate-logs
-    logger.info("Structured logging example.");
+    logger.info("Structured logging.");
     return "ルートディレクトリ起動";
   }
 
@@ -47,11 +63,80 @@ public class WebApi920101Controller {
   @GetMapping("/test")
   public @ResponseBody String test() {
     // Example of structured logging - add custom fields
-    MDC.put("logField", "custom-entry");
-    MDC.put("arbitraryField", "custom-entry");
+    MDC.put("logField", "test");
+    MDC.put("arbitraryField", "任意の領域");
     // Use logger with log correlation
     // https://cloud.google.com/run/docs/logging#correlate-logs
-    logger.info("Structured logging example.");
+    logger.info("Structured logging.");
     return "Hello World!!!!!!!!";
+  }
+
+  /** endpoint handler. */
+  @PostMapping("/")
+  @ResponseBody
+  public LoginMsg getLoginMsg(@RequestHeader Map<String, String> headers, @RequestParam Map<String, String> body) {
+ 
+    LoginMsg rtn = new LoginMsg();
+
+    // Example of structured logging - add custom fields
+    MDC.put("logField", "getLoginMsg");
+    MDC.put("arbitraryField", "■");
+    // Use logger with log correlation
+    // https://cloud.google.com/run/docs/logging#correlate-logs
+    logger.info("Structured logging.getLoginMsg START");
+
+    // Get decoded Id Platform user id
+    String uid = authenticateJwt(headers);
+
+    Date date = new Date();
+    Timestamp timestamp = new Timestamp(date.getTime());
+
+    MDC.put("uid", uid);
+    logger.info("authenticateJwt OK.uid=[" + uid + "]");
+
+    rtn.setMessage("Successfully authentication for " + uid + " at " + timestamp.toLocalDateTime());
+
+    return (rtn);
+//    return "Successfully authentication for " + uid + " at " + timestamp.toLocalDateTime();
+//    return "ルートディレクトリ起動";
+  }
+
+  // [START cloudrun_user_auth_jwt]
+  /** Extract and verify Id Token from header */
+  private String authenticateJwt(Map<String, String> headers) {
+    String authHeader =
+        (headers.get("authorization") != null)
+            ? headers.get("authorization")
+            : headers.get("Authorization");
+    if (authHeader != null) {
+      String idToken = authHeader.split(" ")[1];
+      // If the provided ID token has the correct format, is not expired, and is
+      // properly signed, the method returns the decoded ID token
+      try {
+        FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
+        String uid = decodedToken.getUid();
+        return uid;
+      } catch (FirebaseAuthException e) {
+        logger.error("Error with authentication: " + e.toString());
+        throw new ResponseStatusException(HttpStatus.FORBIDDEN, "", e);
+      }
+    } else {
+      logger.error("Error no authorization header");
+      throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+    }
+  }
+  // [END cloudrun_user_auth_jwt]
+
+  // Message
+  public static class LoginMsg{
+    private String message;
+
+    public String getMessage(){
+      return this.message;
+    }
+
+    public void setMessage(String msg){
+        this.message = msg;
+    }
   }
 }
